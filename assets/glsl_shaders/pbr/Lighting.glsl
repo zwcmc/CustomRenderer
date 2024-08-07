860e9778965965a08d9fe67372bd0d61c9a92a49
chenzwsss <746869297@qq.com>
2024-08-07 21:15:50 +0800

    Add BRDF functions

--- assets/glsl_shaders/pbr/Lighting.glsl
+++ assets/glsl_shaders/pbr/Lighting.glsl
@@ -20,18 +20,28 @@ vec3 PBRLighting(vec3 albedo, vec3 worldNormal, float metallic, float roughness,
     float HdotV = clamp(dot(H, V), 0.0, 1.0);
     float NdotH = clamp(dot(N, H), 0.0, 1.0);
 
-    // cook-torrance brdf
+    vec3 Lo = vec3(0.0);
+
     float D = MicrofacetDistribution(NdotH, roughness);
-    vec3 F = SpecularReflection(HdotV, F0);
+    vec3 F = fresnelSchlick(HdotV, F0);
     float G = GeometricOcclusion(NdotV, NdotL, roughness);
+
+    // // cook-torrance brdf
+    // float D = DistributionGGX(N, H, roughness);
+    // float G = GeometrySmith(N, V, L, roughness);
+    // vec3 F = fresnelSchlick(HdotV, F0);
+
+    vec3 numerator = D * G * F;
+    float denominator = max((4.0 * NdotL * NdotV), 0.001);
+    vec3 BRDF = numerator / denominator;
+
+    Lo += BRDF * radiance * NdotL;
 
     vec3 diffuseTerm = vec3(1.0) - F;
     diffuseTerm *= 1.0 - metallic;
     diffuseTerm = diffuseTerm * albedo / M_PI;
 
-    vec3 specularTerm = D * F * G / (4.0 * NdotV * NdotL + 0.00001);
-
-    vec3 Lo = (diffuseTerm + specularTerm) * radiance * NdotL;
+    Lo += diffuseTerm * radiance * NdotL;
 
     return Lo;
 }
