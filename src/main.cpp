cd6d77fcc10c18817e72bece78c98019370744b3
chenzwsss <746869297@qq.com>
2024-08-23 22:27:11 +0800

    Some optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -136,15 +136,17 @@ int main()
 void checkOpenGLError()
 {
     GLenum err = glGetError();
-    while (err != GL_NO_ERROR) {
+    while (err != GL_NO_ERROR)
+    {
         const char* error;
-        switch (err) {
-        case GL_INVALID_OPERATION:      error = "INVALID_OPERATION"; break;
-        case GL_INVALID_ENUM:           error = "INVALID_ENUM"; break;
-        case GL_INVALID_VALUE:          error = "INVALID_VALUE"; break;
-        case GL_OUT_OF_MEMORY:          error = "OUT_OF_MEMORY"; break;
-        case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-        default:                        error = "UNKNOWN_ERROR"; break;
+        switch (err)
+        {
+            case GL_INVALID_OPERATION: error = "INVALID_OPERATION"; break;
+            case GL_INVALID_ENUM: error = "INVALID_ENUM"; break;
+            case GL_INVALID_VALUE: error = "INVALID_VALUE"; break;
+            case GL_OUT_OF_MEMORY: error = "OUT_OF_MEMORY"; break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+            default: error = "UNKNOWN_ERROR"; break;
         }
         fprintf(stderr, "OpenGL error: %s\n", error);
         err = glGetError();
