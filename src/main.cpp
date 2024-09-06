c5dc15ebd76835349f8650dbca55d87968c2c503
chenzwsss <746869297@qq.com>
2024-09-06 15:23:46 +0800

    Update assets

--- src/main.cpp
+++ src/main.cpp
@@ -100,27 +100,21 @@ int main()
     m_SceneRenderGraph = SceneRenderGraph::New();
     m_SceneRenderGraph->Init();
 
-    SceneNode::Ptr glTFNode = AssetsLoader::LoadModel("models/glTF/DamagedHelmet/glTF/DamagedHelmet.gltf");
+//    SceneNode::Ptr glTFNode = AssetsLoader::LoadModel("models/glTF/DamagedHelmet/glTF/DamagedHelmet.gltf");
 //    SceneNode::Ptr glTFNode = AssetsLoader::LoadModel("models/glTF/buster_drone/busterDrone.gltf");
-//    SceneNode::Ptr glTFNode = AssetsLoader::LoadModel("models/glTF/FlightHelmet/glTF/FlightHelmet.gltf");
-//    SceneNode::Ptr scenglTFNodeeNode = AssetsLoader::LoadModel("models/glTF/DragonAttenuation/glTF/DragonAttenuation.gltf");
-//    glTFNode->Translate(glm::vec3(3.0f, 5.0f, 0.0f));
-    m_SceneRenderGraph->AddSceneNode(glTFNode);
-
-    SceneNode::Ptr marry = AssetsLoader::LoadModel("models/obj/mary/Marry.obj");
-    marry->Translate(glm::vec3(-5.0f, -1.5f, -18.0f));
-    m_SceneRenderGraph->AddSceneNode(marry);
-
-    SceneNode::Ptr ground = AssetsLoader::LoadModel("models/obj/floor/floor.obj");
-    ground->Translate(glm::vec3(0.0f, -1.5f, 0.0f));
-    m_SceneRenderGraph->AddSceneNode(ground);
+//    SceneNode::Ptr glTFNode = AssetsLoader::LoadModel("models/glTF/DragonAttenuation/glTF/DragonAttenuation.gltf");
+//    m_SceneRenderGraph->AddSceneNode(glTFNode);
+    
+    SceneNode::Ptr objNode = AssetsLoader::LoadModel("models/obj/bunny/bunny.obj");
+    objNode->Scale(glm::vec3(10.0f));
+    m_SceneRenderGraph->AddSceneNode(objNode);
 
     // Get the real size in pixels
     int realWidth, realHeight;
     glfwGetFramebufferSize(m_Window, &realWidth, &realHeight);
     glViewport(0, 0, realWidth, realHeight);
     
-    ArcballCamera::Ptr arcballCamera = ArcballCamera::New(glm::vec3(0.0f, 0.0f, 5.5f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    ArcballCamera::Ptr arcballCamera = ArcballCamera::New(glm::vec3(0.0f, 1.0f, 5.5f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     arcballCamera->SetPerspective(glm::radians(45.0f), realWidth, realHeight, 0.1f, 100.0f);
     m_SceneRenderGraph->SetCamera(arcballCamera);
 
